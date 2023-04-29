628521bd440ba950dd4fca638cfacb0fa1abdd92
jocelyn pyw <jocelynwotcheu2@gmail.com>
2023-04-29 13:08:29 +0100

    redesign of main project and generate file complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,23 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import loaderBlue from "./assets/loader_blue.svg";
-import loaderWhite from "./assets/loader_white.svg";
+import logo_editor from "./assets/logo_editor.png";
 import "./App.css";
+import "./styles/FileComponentStyles.css";
 import { FileItem } from "./components/File";
 import { useActions, useSignal } from "@dilane3/gx";
 import { useRef } from "react";
 import { annotateCsv } from "./api";
 import { instance } from "./api";
-import { formatTime } from "./utils";
 import { downloadBaseUrl } from "./utils";
 
 function App() {
-  // Global state
   const files = useSignal("files");
   const { loading, finished, link } = useSignal("loading");
-
-  // Global actions
   const { addFiles } = useActions("files");
   const { start, stop, setLink } = useActions("loading");
-
-  // Local state
   const [zip, setZip] = useState(null);
   const [counter, setCounter] = useState(0);
-
-  // Ref section
   const inputRef = useRef();
 
-  // UseEffect section
   useEffect(() => {
     let timer;
 
@@ -40,59 +29,40 @@ function App() {
 
     return () => clearInterval(timer);
   }, [loading]);
-
-  // Some functions
   const handleOpenFolder = () => {
     if (!loading && inputRef.current) inputRef.current.click();
   };
 
   const handleFileSelected = (event) => {
-    // Add files to global state
     addFiles(event.target.files);
-
-    // Empty files into input tag
-    // inputRef.current.files;
   };
-
   const handleSubmit = async () => {
     if (files.length === 0) return;
-
-    // Start loading
     start();
     setCounter(0);
 
     const { data } = await annotateCsv(files);
 
-    console.log(data)
+    console.log(data);
 
     if (data) {
-      // Set link to download
       setLink(data.link);
     }
-
-    // Stop loading
     stop();
   };
-
   const handleDownload = async (link) => {
     if (!finished) return;
-
-    // Download file with custom name
     const { data } = await instance.get(`/static/${link}`, {
       responseType: "blob",
       headers: {
         "Access-Control-Allow-Origin": "*",
       },
     });
-
-    // Create a blob link to download
     const urlToDownload = window.URL.createObjectURL(new Blob([data]));
 
     const filename = link.split("/").pop();
 
     console.log({ urlToDownload });
-
-    // Add link to download
     setZip({
       url: urlToDownload,
       name: filename,
@@ -101,21 +71,48 @@ function App() {
 
   return (
     <main className="App">
-      <header className="header">
-        <span>.CSV</span>
+      <header className="headerContainer">
+        <div className="headerContainer">
+          <div className="headerTextContainer">
+            <div>
+              <h1 className="headerTitle">SEMANTIC WEB</h1>
+            </div>
+            <div>
+              <h1 className="headerTitle headerTitle1">File Annotation...</h1>
+            </div>
+          </div>
+          <div className="headerLogoContainer">
+            <img src={logo_editor} alt="editor_file_logo" width={70} />
+          </div>
+        </div>
       </header>
 
       <section className="main">
         <div className="main__title">
           <h1>
-            Welcome to <span>CSV ANNOTATOR</span>
+            <strong className="defaulTextColor">how does it work?</strong>
           </h1>
 
-          <p>Upload yours clean CSV files and let us do the rest.</p>
+          <p style={{ textAlign: "center" }}>
+            <strong className="OrangeColor">First (1)</strong> you click on the
+            Add File button <strong className="OrangeColor">Then (2)</strong>{" "}
+            select your files and click OK.
+            <br />
+            <strong className="OrangeColor">Finally (3)</strong> you click on
+            Generate and download the result.
+            <br />
+            For a better experience we recommend you to choose{" "}
+            <strong className="OrangeColor">a maximum of 5</strong> files at a
+            time.
+          </p>
         </div>
 
+        <section className="fileMain">
+          {files.map((file, index) => (
+            <FileItem key={index} file={file} />
+          ))}
+        </section>
         <div className="main__upload">
-          <div className="main__chrono">{formatTime(counter)}</div>
           <div className="main__uploader">
             <input
               ref={inputRef}
@@ -126,81 +123,37 @@ function App() {
               accept="text/csv"
             />
 
-            <button
-              className="main__uploader__title"
-              onClick={handleOpenFolder}
-            >
-              <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M12 5a1 1 0 0 1 1 1v12a1 1 0 1 1-2 0V6a1 1 0 0 1 1-1z"
-                  fill="#000"
-                />
-                <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M5 12a1 1 0 0 1 1-1h12a1 1 0 1 1 0 2H6a1 1 0 0 1-1-1z"
-                  fill="#000"
-                />
-              </svg>
-              <span>Select Files</span>
-
-              <span className="badge">{files.length}</span>
+            <button className="addButton" onClick={handleOpenFolder}>
+              <span>
+                Add Files <i className="plusBtn">+</i>
+              </span>
             </button>
 
             {finished && link !== null ? (
-              <a href={`${downloadBaseUrl}/${link}`} download={link.split("/").pop()} target="_blanc">
+              <a
+                href={`${downloadBaseUrl}/${link}`}
+                download={link.split("/").pop()}
+                target="_blanc"
+              >
                 <button className="main__results">
-                  <svg
-                    width="24"
-                    height="24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
-                      d="M5.293 9.293a1 1 0 0 1 1.414 0L12 14.586l5.293-5.293a1 1 0 1 1 1.414 1.414l-6 6a1 1 0 0 1-1.414 0l-6-6a1 1 0 0 1 0-1.414z"
-                      fill="#000"
-                    />
-                    <path
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
-                      d="M12 3a1 1 0 0 1 1 1v12a1 1 0 1 1-2 0V4a1 1 0 0 1 1-1zM5 20a1 1 0 0 1 1-1h12a1 1 0 1 1 0 2H6a1 1 0 0 1-1-1z"
-                      fill="#000"
-                    />
-                  </svg>
                   <span>Download</span>
                 </button>
               </a>
             ) : (
-              <span className="tips">
-                Use Ctrl or Shift to select many files
+              <span className="tipiis">
+                {/* Use Ctrl or Shift to select many files */}
               </span>
             )}
 
             <button
-              className={`upload__btn ${files.length > 0 ? "active" : ""}`}
+              className={`uploadBtn ${files.length > 0 ? "activee" : ""}`}
               onClick={handleSubmit}
             >
-              <span>Annotate</span>
+              <span>Generate</span>
 
-              {loading ? (
-                <img src={loaderWhite} width="24" height="24" />
-              ) : (
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
-                  <path d="M11.293 4.707 17.586 11H4v2h13.586l-6.293 6.293 1.414 1.414L21.414 12l-8.707-8.707-1.414 1.414z" />
-                </svg>
-              )}
+              {loading ? " ..." : ""}
             </button>
           </div>
-
-          <section className="main__files">
-            {files.map((file, index) => (
-              <FileItem key={index} file={file} />
-            ))}
-          </section>
         </div>
       </section>
 
--- src/components/File.jsx
+++ src/components/File.jsx
@@ -1,7 +1,9 @@
 import styles from "./styles/fileItem.module.css";
-import csvLogo from "../assets/csv.png";
+import "../styles/FileComponentStyles.css";
 import { useAction, useSignal } from "@dilane3/gx";
 import { formatSize } from "../utils";
+import logo from "../assets/csv_annotation.png";
+import deleteBtn from "../assets/delete.png";
 
 export const FileItem = ({ file }) => {
   // Global action
@@ -18,23 +20,19 @@ export const FileItem = ({ file }) => {
   };
 
   return (
-    <div className={styles.file__item}>
+    <div className="fileItem">
       <div className={styles.file__info}>
-        <img src={csvLogo} width={25} className={styles.file__image} />
-
-        <span className={styles.file__name}>{file.name}</span>
+        <img src={logo} width={125} />
       </div>
 
-      <div className={styles.file__controls}>
-        <span className={styles.file__size}>{formatSize(file.size)}</span>
-
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          onClick={handleDelete}
-        >
-          <path d="M12,2A10,10,0,1,0,22,12,10,10,0,0,0,12,2Zm3.707,12.293a1,1,0,1,1-1.414,1.414L12,13.414,9.707,15.707a1,1,0,0,1-1.414-1.414L10.586,12,8.293,9.707A1,1,0,0,1,9.707,8.293L12,10.586l2.293-2.293a1,1,0,0,1,1.414,1.414L13.414,12Z" />
-        </svg>
+      <div>
+        <span className="fileSize">{formatSize(file.size)}</span>
+      </div>
+      <div>
+        <span className="fileName">{file.name}</span>
+      </div>
+      <div onClick={handleDelete}>
+        <img className="delete_icon" src={deleteBtn} />
       </div>
     </div>
   );
